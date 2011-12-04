3a738a1b471a4c891fb4be60da821bcb9b0f764a
Emanuele Rampichini <emanuele@Emanuele-Rampichini-Macbook-Pro.local>
2011-12-04 23:41:25 +0100

    Ascii world manipulator test added

---
+++ tests/AsciiWorldManipulatorTests.java
@@ -0,0 +1,25 @@
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+
+public class AsciiWorldManipulatorTests {
+
+	@Test
+	public void testCanGenerateWorldFromAsciiRepresentation()
+	{
+		String asciiWorldRepresentation = 
+				".o.\n" +
+				".o.\n" +
+				".o.\n";
+		World world =  new World();
+		AsciiWorldManipulator worldManipulator = new AsciiWorldManipulator(); 
+		
+		worldManipulator.populate(world, asciiWorldRepresentation);
+		Assert.assertTrue(world.cellCount() == 3);
+		Assert.assertTrue(world.containsCellAt(1, 0));
+		Assert.assertTrue(world.containsCellAt(1, 1));
+		Assert.assertTrue(world.containsCellAt(1, 2));
+	}
+	
+}
