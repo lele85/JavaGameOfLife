1d2c9340810418ec0ba0b9533e2c21b4d6674705
Emanuele Rampichini <emanuele@Emanuele-Rampichini-Macbook-Pro.local>
2011-12-04 23:40:54 +0100

    Ascii world manupulator

---
+++ src/AsciiWorldManipulator.java
@@ -0,0 +1,23 @@
+
+public class AsciiWorldManipulator {
+
+	final char aliveCellChar = 'o';
+	final char lineSeparator = '\n';
+	
+	public void populate(World world, String asciiWorldRepresentation) {
+		int x = 0;
+		int y = 0;
+		for (char currentChar:asciiWorldRepresentation.toCharArray())
+		{
+			if (currentChar == aliveCellChar){
+				world.addCellAt(x, y);
+			}
+			x++;
+			if (currentChar == lineSeparator){
+				x = 0;
+				y += 1;
+			}
+		}
+	}
+	
+}
